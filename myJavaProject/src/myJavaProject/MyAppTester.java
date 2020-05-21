539a5558e6bbec2574693a6c53e62aba61150e19
Hyeonju <aqua8410@gmail.com>
2020-05-20 18:22:50 -0700

    commit MyAppTester class, on b1 branch

--- myJavaProject/src/myJavaProject/MyAppTester.java
+++ myJavaProject/src/myJavaProject/MyAppTester.java
@@ -20,8 +20,16 @@ public class MyAppTester {
 		System.out.println("Enter a number for calculating factorial");
 		int num = sc.nextInt();
 		System.out.println(num + "! = " + getFactorial(num));
+		/*
+		 * Git now is on branch b1
+		 * From now on until switching out of branch b1
+		 * any changes occur here will be tracked in branch b1
+		 * and will not be reflected in any other branch unless b1
+		 * is merged later with that branch
+		 * */
 	}
-
+	
+	//This is a method to get Factorial
 	public static int getFactorial(int n) {
 		int result = 0;
 		if (n == 0)
